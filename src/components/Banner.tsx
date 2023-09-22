ff670cad3c22f4d94fa37f187f188ee83aab7dc3
Paulo Turino <paulo.turino01@gmail.com>
2023-09-22 11:31:40 -0300

    adicionando link do cv no botao correspondente

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -11,7 +11,7 @@ export default function Banner() {
                 <h1 className="text-5xl md:text-6xl font-bold">Paulo Turino Silva</h1>
                 <h2 className="text-3xl md:text-4xl font-bold">Desenvolvedor Frontend</h2>
                 <div className="flex gap-2 md:gap-4 mt-10 items-center">
-                    <button className={`button-gradient text-xl font-bold py-2 px-4 md:mr-10 border border-transparent transition-color ${theme === 'dark' && "button-gradient-dark"}`}>Currículo</button>
+                    <a href="https://drive.google.com/file/d/1WymOGaZioacMD8DYMKgMAJr1IzmkWqJQ/view?usp=sharing" target="_blank" className={`button-gradient text-xl font-bold py-2 px-4 md:mr-10 border border-transparent transition-color ${theme === 'dark' && "button-gradient-dark"}`}>Currículo</a>
                     <a href="https://www.linkedin.com/in/paulo-turino/" target="_blank" className="text-6xl duration-300 hover:-translate-y-2 hover:scale-110"><AiFillLinkedin /></a>
                     <a id="about" href="https://github.com/TurinoS" target="_blank" className="text-6xl duration-300 hover:-translate-y-2 hover:scale-110"><AiFillGithub /></a>
                 </div>
